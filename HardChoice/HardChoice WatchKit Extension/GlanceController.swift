70a7392ad5ee7b14de9b76626eff96b16f1e5ded
yulingtianxia <yulingtianxia@gmail.com>
2015-04-08 23:53:46 +0800

    添加Glance（待完成）

---
+++ HardChoice/HardChoice WatchKit Extension/GlanceController.swift
@@ -0,0 +1,15 @@
+//
+//  GlanceController.swift
+//  HardChoice
+//
+//  Created by 杨萧玉 on 15/4/8.
+//  Copyright (c) 2015年 杨萧玉. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import DataKit
+
+class GlanceController: WKInterfaceController {
+
+}
